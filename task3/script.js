3f6faf5b0ec25538f8306f83fb2c589d4696dfe6
Rahul2k06 <rahulvijayendran006@gmail.com>
2026-07-02 12:26:16 +0530

    Add files via upload

---
+++ task3/script.js
@@ -0,0 +1,9 @@
+// Portfolio Website JavaScript
+
+console.log("Portfolio Website Loaded Successfully");
+
+// Simple animation alert
+
+window.addEventListener("scroll", () => {
+    console.log("Scrolling...");
+});
